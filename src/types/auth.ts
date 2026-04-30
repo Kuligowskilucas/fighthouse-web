aa84fdae3666a7a21b7c2a60c2e3b4ec82a13f92
Lucas Gabriel Kuligowski <lucaskuligowski@gmail.com>
2026-04-30 18:14:38 -0300

    feat(auth): setup axios, types e instalar componentes shadcn

---
+++ src/types/auth.ts
@@ -0,0 +1,20 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  user: User;
+  token: string;
+}
+
+export interface LaravelValidationError {
+  message: string;
+  errors: Record<string, string[]>;
+}
